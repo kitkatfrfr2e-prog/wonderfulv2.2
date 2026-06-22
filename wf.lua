8d091240736caf2200381440510e63e64f70cf22
kitkatfrfr2e-prog <kitkatfrfr2e@gmail.com>
2026-06-22 09:38:02 +0300

    Update wf.lua

--- wf.lua
+++ wf.lua
@@ -1564,7 +1564,7 @@ do
             Min     = Info.Min;
             Max     = Info.Max;
             Rounding = Info.Rounding;
-            MaxSize  = 330;
+            MaxSize  = 323;
             Type     = 'Slider';
             Callback = Info.Callback or function() end;
         }
